06210e45c73b8a317384e71def21b098c4d3aaef
delstevens <apprentice@StMac0.local>
2015-08-02 18:04:55 +1200

    add signup fields and styling

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+post '/users' do
+  user = User.create(params[:user])
+  session[:user_id] = user.id
+  redirect '/'
+end
+
+get '/signup' do
+  erb :signup
+end
--- public/js/application.js
+++ public/js/application.js
@@ -5,3 +5,22 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+});
